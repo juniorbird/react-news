967bfd6e62e169ed65a40a4ee73b1fb1554bfdb3
Wade Armstrong <wade@wadearmstrong.com>
2016-05-06 11:55:12 -0700

    update to my Firebase

--- src/js/util/constants.js
+++ src/js/util/constants.js
@@ -13,4 +13,4 @@ export const errorMessages = {
     generic: 'Something went wrong.'
 };
 
-export const firebaseUrl = 'https://resplendent-fire-4810.firebaseio.com/';
+export const firebaseUrl = 'https://scorching-fire-5787.firebaseio.com/';
