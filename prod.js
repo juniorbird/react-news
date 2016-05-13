074031b6439130f5883d27e1ce976dc81e787ead
Wade Armstrong <wade@wadearmstrong.com>
2016-05-13 13:35:20 -0700

    fix prod to not try to send headers multiple times, which I should know better than to do

--- prod.js
+++ prod.js
@@ -2,15 +2,23 @@
 const http = require('http');
 const fs = require('fs');
 
+let file;
+let header;
+
+
 http.createServer((request, response) => {
   if (request.url.substring(1) === 'app.min.js') {
-    response.writeHead(200, { 'Content-Type': 'text/html' });
-    fs.readFile('./dist/app.min.js', (error, data) => response.end(error || data));
+    header = { 'Content-Type': 'text/html' };
+    file = './dist/app.min.js';
   } else if (request.url.substring(1) === 'favicon.png') {
-    response.writeHead(200, { 'Content-Type': 'image/png' });
-    fs.readFile('favicon.png', (error, data) => response.end(error || data));
+    header = { 'Content-Type': 'image/png' };
+    file = './favicon.png';
   } else {
-    response.writeHead(200, { 'Content-Type': 'text/html' });
-    fs.readFile('./dist/index.html', (error, data) => response.end(error || data));
+    header =  { 'Content-Type': 'text/html' };
+    file = './dist/index.html';
   }
+
+  response.writeHead(200,  header);
+  fs.readFile(file, (error, data) => response.end(error || data));
+  
 }).listen(process.env.PORT || 3000);
