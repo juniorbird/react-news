bdea6001853e7dbcf2bca8d519b4f1148d1e819c
Wade Armstrong <wade@wadearmstrong.com>
2016-05-13 13:22:38 -0700

    fix wrong contenttype for FAVICON AGAINNNNNN

--- prod.js
+++ prod.js
@@ -3,12 +3,14 @@ const http = require('http');
 const fs = require('fs');
 
 http.createServer((request, response) => {
-  response.writeHead(200, { 'Content-Type': 'text/html' });
   if (request.url.substring(1) === 'app.min.js') {
+    response.writeHead(200, { 'Content-Type': 'text/html' });
     fs.readFile('./dist/app.min.js', (error, data) => response.end(error || data));
   } else if (request.url.substring(1) === 'favicon.png') {
+    response.writeHead(200, { 'Content-Type': 'image/png' });
     fs.readFile('favicon.png', (error, data) => response.end(error || data));
   } else {
+    response.writeHead(200, { 'Content-Type': 'text/html' });
     fs.readFile('./dist/index.html', (error, data) => response.end(error || data));
   }
 }).listen(process.env.PORT || 3000);
